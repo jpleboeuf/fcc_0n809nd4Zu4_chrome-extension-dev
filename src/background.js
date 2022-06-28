3a92be33e6a788f701493bd63615985269174b80
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2022-06-28 20:36:59 +0200

    create the basic version of the extension (cont.)
    In the service worker, fixed the listener for `chrome.tabs.onUpdated`.
    
    The author of the tutorial was conflating
     the `changeInfo` and `tab` parameters,
     by not providing the 3rd parameter to the listener.
    
    By fixing this mistake, it made also possible to
     beautifully handle both the load and the reload of web pages.

--- src/background.js
+++ src/background.js
@@ -1,7 +1,27 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    console.group("chrome.tabs.onUpdated")
-    console.log("tab.url:= " + tab.url);
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    /*
+      + `changeInfo.url` / `tab.url`:
+        - if `changeInfo.url` is `undefined`, it means that the URL did not change,
+           the current URL is still the one in `tab.url`;
+        - if `changeInfo.url` is a URL, it means that the URL did change,
+           as reflected in `tab.url`.
+      + `changeInfo.status` / `tab.status`:
+        - if `changeInfo.status` is `undefined`, it means that the status did not change,
+           the current status is still the one in `tab.status`;
+        - if `changeInfo.url` is a status, it means that the status did change,
+           as reflected in `tab.status`.
+    */
+    console.group("chrome.tabs.onUpdated");
+
+    let tabURLChangedIndicator;
+    changeInfo.url !== undefined ? (tabURLChangedIndicator = "*") : (tabURLChangedIndicator = "");
+    let tabStatusChangedIndicator;
+    changeInfo.status !== undefined ? (tabStatusChangedIndicator = "*") : (tabStatusChangedIndicator= "");
+    console.log("tab.url (tab.status) {changed values prefixed with *}:= "
+        + tabURLChangedIndicator + tab.url + " (" + tabStatusChangedIndicator + tab.status + ")");
+
+    if (changeInfo.status === "loading" && tab.url.includes("youtube.com/watch")) {
+        /* Loading or reloading a YouTube video page changes `changeInfo.status` to `loading`. */
 
         const urlQueryString = tab.url.split("?")[1];
         const urlParameters = new URLSearchParams(urlQueryString);
@@ -30,5 +50,6 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
         );
 
     }
+
     console.groupEnd();
 });
