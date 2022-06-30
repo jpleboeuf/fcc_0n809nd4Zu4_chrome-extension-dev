104439d1606b717d9dbb22abe46f00bc6d60a27e
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2022-06-30 14:19:03 +0200

    add the bookmarking button
    Added the bookmarking button
     at the right of the "left" controls
     of the YouTube video player.
    
    When the bookmarking button is clicked,
     the current time in the YouTube video is diplayed in the console.

--- src/contentScript.js
+++ src/contentScript.js
@@ -4,6 +4,8 @@ console.log("content-script!");
 
 let currentVideo = "";
 
+let ytpVideo, ytpLeftControls;
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const { type, videoId } = request;
     if (type === "NEW") {
@@ -16,7 +18,36 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 const newYouTubeVideoPage = () => {
     console.group("newYouTubeVideoPage");
     console.log("currentVideo:= " + currentVideo);
+    document.addEventListener("DOMContentLoaded", newYouTubeVideoPageReadyEventHandler, false);
     console.groupEnd();
 };
+
+const newYouTubeVideoPageReadyEventHandler = () => {
+    console.group("newYouTubeVideoPageReadyEventHandler");
+
+    ytpVideo = document.getElementsByClassName("video-stream")[0];
+    //console.log(ytpVideo);
+
+    const bookmarkBtn = document.createElement("img");
+    bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    bookmarkBtn.className = "ytp-button" /* base class for YT buttons */ + " " + "bookmark-btn";
+    bookmarkBtn.title = "Bookmark current timestamp";
+    //console.log(bookmarkBtn)
+
+    ytpLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    //console.log(ytpLeftControls);
+
+    ytpLeftControls.appendChild(bookmarkBtn);
+
+    bookmarkBtn.addEventListener("click", addBookmarkEventHandler);
+    console.groupEnd();
+}
+
+const addBookmarkEventHandler = () => {
+    console.group("addBookmarkEventHandler");
+    let currentVideoTime = ytpVideo.currentTime;
+    console.log("currentVideoTime:= " + currentVideoTime);
+    console.groupEnd();
+}
 
 //})();
